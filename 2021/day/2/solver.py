fc918053c7c68711cbce611ce865185bdf8f84d4
Aonghus Haicéid <gus@kakahi.org>
2021-12-02 12:10:15 +0000

    2021 day 2 nip/tuck

--- 2021/day/2/solver.py
+++ 2021/day/2/solver.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
 
-OPERATORS = {
+MACHINE1 = {
+    "init":     (0,0),
     "forward":  lambda op, st: (st[0] + op, st[1]),
     "down":     lambda op, st: (st[0], st[1] + op),
     "up":       lambda op, st: (st[0], st[1] - op),
 }
-INITIAL_STATE = (0,0)
 
-OPERATORS2 = {
+MACHINE2 = {
+    "init":     (0,0,0),
     "forward":  lambda op, st: (st[0] + op, st[1] + st[2] * op, st[2]),
     "down":     lambda op, st: (st[0], st[1], st[2] + op),
     "up":       lambda op, st: (st[0], st[1], st[2] - op),
 }
-INITIAL_STATE2 = (0,0,0)
 
 class LocationMachine:
 
-    def __init__(self, operators, state):
+    def __init__(self, operators):
         self.operators = operators
-        self.state = state
+        self.state = self.operators["init"]
 
     def process(self, input):
         self.state = self.operators[input[0]](int(input[1]), self.state)
 
 if __name__ == "__main__":
-    lm = LocationMachine(OPERATORS, INITIAL_STATE)
-    lm2 = LocationMachine(OPERATORS2, INITIAL_STATE2)
+    lm = LocationMachine(MACHINE1)
+    lm2 = LocationMachine(MACHINE2)
     with open("input") as fh:
         for line in fh.readlines():
             lm.process(line.strip().split())
