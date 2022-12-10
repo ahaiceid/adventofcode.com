9a44691e495a41a203c0d4b473a3a8c7eae70aa6
Aonghus Haicéid <93676994+ahaiceid@users.noreply.github.com>
2022-12-10 16:49:01 +0000

    Day 9 Part 2

--- 2022/day/9/solver.py
+++ 2022/day/9/solver.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 
+
 class RopeBridge:
 
     direction_to_cartesian = {
@@ -12,60 +13,75 @@ class RopeBridge:
 
     displacement_to_tail_move = {
         # map of head displacement to tail move
+        (-2,-2): (-1,-1),
         (-2,-1): (-1,-1),
         (-2,0): (-1,0),
         (-2,1): (-1,1),
+        (-2,2): (-1,1),
         (-1,-2): (-1,-1),
         (-1,2): (-1,1),
         (0,-2): (0,-1),
         (0,2): (0,1),
         (1,-2): (1,-1),
         (1,2): (1,1),
+        (2,-2): (1,-1),
         (2,-1): (1,-1),
         (2,0): (1,0),
         (2,1): (1,1),
+        (2,2): (1,1),
         # if a delta isn't here do nothing
     }
 
-    def __init__(self, head_position=(0,0), tail_position=(0,0)):
-        self.initialize(head_position, tail_position)
+    def __init__(self, positions=[(0,0),(0,0)]):
+        self.initialize(positions)
 
-    def initialize(self, head_position, tail_position):
-        self.head_position = list(head_position)
-        self.tail_position = list(tail_position)
+    def initialize(self, positions):
+        self.positions = [[x,y] for x,y in positions]
 
     def move_head(self, direction):
         cartesian = self.direction_to_cartesian[direction]
-        self.head_position[0] += cartesian[0]
-        self.head_position[1] += cartesian[1]
+        self.positions[0][0] += cartesian[0]
+        self.positions[0][1] += cartesian[1]
 
     def calculate_tail_move(self, displacement):
         return self.displacement_to_tail_move.get(displacement,(0,0))
 
-    def update_tail(self):
+    def update_tail(self, tail_index):
         displacement = (
-            self.head_position[0] - self.tail_position[0],
-            self.head_position[1] - self.tail_position[1])
+            self.positions[tail_index-1][0] - self.positions[tail_index][0],
+            self.positions[tail_index-1][1] - self.positions[tail_index][1])
         update = self.calculate_tail_move(displacement)
-        #print('{} -> {},'.format(displacement,update))
-        self.tail_position[0] += update[0]
-        self.tail_position[1] += update[1]
+        self.positions[tail_index][0] += update[0]
+        self.positions[tail_index][1] += update[1]
+
+    def update_tails(self):
+        for tail_index in range(1,len(self.positions)):
+            self.update_tail(tail_index)
 
 def part1(input_data):
     rope_bridge = RopeBridge()
-    visited = set(tuple(rope_bridge.tail_position))
+    visited = set()
     for move_descriptor in input_data:
         move_direction, move_count = move_descriptor.split()
         for _ in range(int(move_count)):
             rope_bridge.move_head(move_direction)
-            rope_bridge.update_tail()
-            visited.add(tuple(rope_bridge.tail_position))
-            print(len(visited))
+            rope_bridge.update_tails()
+            visited.add(tuple(rope_bridge.positions[1]))
     return len(visited)
 
 def part2(input_data):
-    pass
+    rope_bridge = RopeBridge(((0,0),)*10)
+    visited = set()
+    for move_descriptor in input_data:
+        move_direction, move_count =move_descriptor.split()
+        for _ in range(int(move_count)):
+            rope_bridge.move_head(move_direction)
+            rope_bridge.update_tails()
+            visited.add(tuple(rope_bridge.positions[9]))
+    return len(visited)
 
 if __name__ == '__main__':
     with open('input', 'r') as data:
         print(part1(data))
+    with open('input', 'r') as data:
+        print(part2(data))
--- 2022/day/9/tests.py
+++ 2022/day/9/tests.py
@@ -214,41 +214,36 @@ class TestSolver(unittest.TestCase):
     def test_moves(self):
         for state_before, move_descriptor, state_after in self.move_cases:
             with self.subTest(msg=move_descriptor):
-                head_before, tail_before = decode_state(state_before)
-                head_after, tail_after = decode_state(state_after)
+                positions_before = decode_state(state_before)
+                positions_after = decode_state(state_after)
                 move_direction, move_count = move_descriptor.split()
-                rope_bridge = solver.RopeBridge(head_before, tail_before)
+                rope_bridge = solver.RopeBridge(positions_before)
                 for _ in range(int(move_count)):
                     rope_bridge.move_head(move_direction)
-                    rope_bridge.update_tail()
-                self.assertEqual(tuple(rope_bridge.head_position), head_after)
-                self.assertEqual(tuple(rope_bridge.tail_position), tail_after)
-                self.assertEqual(rope_bridge.head_position[0], head_after[0])
-                self.assertEqual(rope_bridge.head_position[1], head_after[1])
-                self.assertEqual(rope_bridge.tail_position[0], tail_after[0])
-                self.assertEqual(rope_bridge.tail_position[1], tail_after[1])
+                    rope_bridge.update_tails()
+                self.assertEqual(tuple(rope_bridge.positions[0]), positions_after[0])
+                self.assertEqual(tuple(rope_bridge.positions[1]), positions_after[1])
 
     def test_tail_update(self):
         for state_before, state_after in self.tail_update_cases:
             with self.subTest(state_before):
-                rope_bridge = solver.RopeBridge(*decode_state(state_before))
-                rope_bridge.update_tail()
+                rope_bridge = solver.RopeBridge(decode_state(state_before))
+                rope_bridge.update_tails()
                 head_after, tail_after = decode_state(state_after)
-                self.assertEqual(tuple(rope_bridge.head_position), head_after)
-                self.assertEqual(tuple(rope_bridge.tail_position), tail_after)
+                self.assertEqual(tuple(rope_bridge.positions[0]), head_after)
+                self.assertEqual(tuple(rope_bridge.positions[1]), tail_after)
 
 
 
     def test_visited(self):
         visited_positions = set()
         rope_bridge = solver.RopeBridge()
-        visited_positions.add(tuple(rope_bridge.tail_position))
         for move_descriptor in self.visited_case[0]:
             move_direction, move_count = move_descriptor.split()
             for _ in range(int(move_count)):
                 rope_bridge.move_head(move_direction)
-                rope_bridge.update_tail()
-                visited_positions.add(tuple(rope_bridge.tail_position))
+                rope_bridge.update_tails()
+                visited_positions.add(tuple(rope_bridge.positions[1]))
         expected_visited = decode_visited(self.visited_case[1])
         self.assertSetEqual(visited_positions, expected_visited)
         self.assertEqual(len(visited_positions), self.visited_case[2])
