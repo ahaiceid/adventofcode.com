0c8df76a547308fa0da05599c6d46d30899b62b8
Aonghus Haicéid <93676994+ahaiceid@users.noreply.github.com>
2022-12-15 14:05:39 +0000

    Day 15 Solution, exploring Moore's Law.

--- 2022/day/15/solver.py
+++ 2022/day/15/solver.py
@@ -29,6 +29,9 @@ class SensorDetectionChecker:
     def _compute_cardinal_distance(self, p1, p2) -> int:
         return abs(p2[0] - p1[0]) + abs(p2[1] - p1[1])
 
+def compute_cardinal_distance(p1, p2):
+    return abs(p2[0]-p1[0]) + abs(p2[1]-p1[1])
+
 def calculate_bounding_area(sensor_detection_checkers):
     min_x = min([sensor_detection_checker.get_bounding_area()[0][0] for sensor_detection_checker in sensor_detection_checkers])
     min_y = min([sensor_detection_checker.get_bounding_area()[0][1] for sensor_detection_checker in sensor_detection_checkers])
@@ -53,24 +56,51 @@ def part1(input_data, row_index=2000000):
     beacon_positions = [data[1] for data in sensor_data]
     min_pos, max_pos = calculate_bounding_area(sensor_detection_checkers)
     positions = [(x,row_index) for x in range(min_pos[0],max_pos[0]+1)]
-    print('Checking {} positions'.format(len(positions)))
     return len(check_positions(sensor_detection_checkers,beacon_positions,positions))
+
+
+def detector_perimeter_position_generator(sensor_detector_checker):
+    centre = sensor_detector_checker.centre
+    distance = sensor_detector_checker.distance + 1
+    points = [
+        (centre[0] + distance, centre[1]),
+        (centre[0], centre[1] + distance),
+        (centre[0] - distance, centre[1]),
+        (centre[0], centre[1] - distance),
+    ]
+    position = points[0]
+    while position != points[1]:
+        position = position[0] - 1, position[1] + 1
+        yield position
+    while position != points[2]:
+        position = position[0] - 1, position[1] - 1
+        yield position
+    while position != points[3]:
+        position = position[0] + 1, position[1] - 1
+        yield position
+    while position != points[0]:
+        position = position[0] + 1, position[1] + 1
+        yield position
+
+class PositionNotSuitable(Exception):
+    pass
 
 def part2(input_data, max_coord=4000000):
     sensor_data = decode_sensor_reports(input_data)
     sensor_detection_checkers = [SensorDetectionChecker(*data) for data in sensor_data]
-    #beacon_positions = [data[1] for data in sensor_data]
-    #min_pos, max_pos = calculate_bounding_area(sensor_detection_checkers)
-    for position in generate_reverse_onion(max_coord):
-        class PositionNotSuitable(Exception):
-            pass
-        try:
-            for sensor_detection_checker in sensor_detection_checkers:
-                if sensor_detection_checker.is_within_range(position):
-                    raise PositionNotSuitable()
-        except PositionNotSuitable:
-            continue
-        return position[0]*4000000+position[1]
+    for sensor_detection_checker in sensor_detection_checkers:
+        for position in detector_perimeter_position_generator(sensor_detection_checker):
+            if min(position[0],position[1]) < 0:
+                continue
+            if max(position[0],position[1]) > max_coord:
+                continue
+            try:
+                for sensor_detection_checker_ in sensor_detection_checkers:
+                    if sensor_detection_checker_.is_within_range(position):
+                        raise PositionNotSuitable()
+            except PositionNotSuitable:
+                continue
+            return position[0]*4000000+position[1]
 
 if __name__ == '__main__':
     with open('input', 'r') as data:
--- 2022/day/15/tests.py
+++ 2022/day/15/tests.py
@@ -68,6 +68,17 @@ class TestSolver(unittest.TestCase):
                               (18, 10), (19, 10), (20, 10), (21, 10), (22, 10), (23, 10), (24, 10)]
         self.assertEqual(solver.check_positions(sensor_detection_checkers,beacon_positions,positions),expected_positions)
 
+    def test_detector_perimeter_position_generator(self):
+        sensor_detector_checker = solver.SensorDetectionChecker(*self.sample_sensor_reports[6])
+        positions = [x for x in solver.detector_perimeter_position_generator(sensor_detector_checker)]
+        expected_positions = [(17, 8), (16, 9), (15, 10), (14, 11), (13, 12), (12, 13), (11, 14),
+                              (10, 15), (9, 16), (8, 17), (7, 16), (6, 15), (5, 14), (4, 13),
+                              (3, 12), (2, 11), (1, 10), (0, 9), (-1, 8), (-2, 7), (-1, 6), (0, 5),
+                              (1, 4), (2, 3), (3, 2), (4, 1), (5, 0), (6, -1), (7, -2), (8, -3),
+                              (9, -2), (10, -1), (11, 0), (12, 1), (13, 2), (14, 3), (15, 4),
+                              (16, 5), (17, 6), (18, 7)]
+        self.assertEqual(positions, expected_positions)
+
     def test_part1(self):
         self.assertEqual(solver.part1(self.sample_data, 10), 26)
 
