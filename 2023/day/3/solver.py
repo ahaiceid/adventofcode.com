a81ea757f49dc820850ca75f7a485ce874dab9ea
Aonghus Haicéid <ahaiceid-github@kakahi.org>
2023-12-08 16:13:13 +0000

    Day 3 Part 1

---
+++ 2023/day/3/solver.py
@@ -0,0 +1,46 @@
+#/usr/bin/python3
+
+__digits = ['{}'.format(x) for x in range(10)]
+__neighbour_offsets = [(-1,-1),(0,-1),(1,-1),(-1,0),(1,0),(-1,1),(0,1),(1,1)]
+
+def part1(input_data):
+    schematic = {}
+    numbers = []
+    symbols = []
+    for y, line in enumerate(input_data):
+        schematic[y] = {}
+        number_in_progress = False
+        for x, ch in enumerate(line.strip()):
+            if ch == '.':
+                number_in_progress = False
+            elif ch in __digits:
+                if number_in_progress:
+                    numbers[-1] = numbers[-1] * 10 + int(ch)
+                else:
+                    number_in_progress = True
+                    numbers.append(int(ch))
+                schematic[y][x] = (0, len(numbers)-1)
+            else:
+                number_in_progress = False
+                symbols.append((ch,(x,y)))
+                schematic[y][x] = (1, len(symbols)-1)
+    part_numbers = set([])
+    for symbol in symbols:
+        symbol, pos = symbol
+        for neighbour_offset in __neighbour_offsets:
+            try:
+                location = schematic[pos[1]+neighbour_offset[1]][pos[0]+neighbour_offset[0]]
+                if location[0] == 0:
+                    part_numbers.add(location[1])
+            except KeyError:
+                pass
+    return sum([numbers[n] for n in part_numbers])
+
+def part2(input_data):
+    raise NotImplementedError
+
+if __name__=="__main__":
+    with open('input') as input_data:
+        print(part1(input_data))
+    with open('input') as input_data:
+        print(part2(input_data))
---
+++ 2023/day/3/tests.py
@@ -0,0 +1,25 @@
+#/usr/bin/python3
+
+import unittest
+import solver
+
+class TestSolver(unittest.TestCase):
+
+    sample_data = [
+        '467..114..',
+        '...*......',
+        '..35..633.',
+        '......#...',
+        '617*......',
+        '.....+.58.',
+        '..592.....',
+        '......755.',
+        '...$.*....',
+        '.664.598..']
+
+
+    def test_part1(self):
+        self.assertEqual(solver.part1(self.sample_data), 4361)
+
+    def test_part2(self):
+        self.assertRaises(NotImplementedError)
