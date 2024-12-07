3cb1f8a3e2a0eb447c56214c5c80dd5f02384c8e
Aonghus Haicéid <gus@kakahi.org>
2024-12-07 13:59:50 +0000

    Day 6. Removed non-functional part 2 stuff.

--- 2024/day/6/solver.py
+++ 2024/day/6/solver.py
@@ -11,47 +11,34 @@ def test_map_bounds(map, pos):
     if pos[0] == len(map): raise StopIteration("")
     if pos[1] == len(map[0]): raise StopIteration("")
 
-def advance(map, pos, fwd, obstruction_count, history):
+def advance(map, pos, fwd):
     map[pos[1]][pos[0]] = 'X'
     while True:
         next_pos = get_next_pos(pos, fwd)
         test_map_bounds(map,next_pos)
         if map[next_pos[1]][next_pos[0]] != "#":
             pos = next_pos
-            if history:
-                try:
-                    if history[0]==history[2] and history[1]<=history[3]:
-                        obstruction_count += 1
-                except IndexError:
-                    pass
-                history[0]+=1
             break
         fwd = turn_right(fwd)
-        if history:
-            history=[0]+history[:3]
 
-    return map, pos, fwd, obstruction_count, history
+    return map, pos, fwd
 
 def part1(input_data):
     map = [[c for c in s.strip()] for s in input_data]
     pos = None
     fwd = (0,-1)
-    obstruction_count = 0
-    history = [0]
     for y,r in enumerate(map):
         for x,c in enumerate(r):
             if c=='^':
                 pos = (x,y)
                 break
-    import pdb; pdb.set_trace()
     try:
         while True:
             #print('\n'.join([''.join(r) for r in map]),end="\n\n")
-            map, pos, fwd, obstruction_count, history = advance(map, pos, fwd, obstruction_count, history)
+            map, pos, fwd = advance(map, pos, fwd)
     except StopIteration:
         pass
-    return sum((True for r in map for cell in r if cell=='X')), obstruction_count
-    return -1
+    return sum((True for r in map for cell in r if cell=='X'))
 
 
 if __name__=="__main__":
--- 2024/day/6/tests.py
+++ 2024/day/6/tests.py
@@ -6,9 +6,5 @@ class TestSolver(unittest.TestCase):
     sample_data = ['....#.....', '.........#', '..........', '..#.......', '.......#..', '..........', '.#..^.....', '........#.', '#.........', '......#...']
 
     def test_part1(self):
-        self.assertEqual(solver.part1(self.sample_data), (41,6))
-
-    @unittest.skip("not yet")
-    def test_part2(self):
-        pass
+        self.assertEqual(solver.part1(self.sample_data), 41)
 
